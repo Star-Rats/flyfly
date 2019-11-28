387ef0faf663c950fb45aa90f9773ca635a58016
Star-Rats <54135730+Star-Rats@users.noreply.github.com>
2019-11-28 17:20:29 +0800

    Create Big1907FlyApplication.java
    fly社区开源项目

---
+++ src/main/java/com/jmy/Big1907FlyApplication.java
@@ -0,0 +1,13 @@
+package com.jmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Big1907FlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Big1907FlyApplication.class, args);
+    }
+
+}
