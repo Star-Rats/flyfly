7581736e0aaccfc3d361826dd7aa1760b8201925
Star-Rats <54135730+Star-Rats@users.noreply.github.com>
2019-12-10 18:27:28 +0800

    Update and rename Big1907FlyApplication.java to FlyApplication.java

--- src/main/java/com/jmy/FlyApplication.java
+++ src/main/java/com/jmy/FlyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Big1907FlyApplication {
+public class FlyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Big1907FlyApplication.class, args);
