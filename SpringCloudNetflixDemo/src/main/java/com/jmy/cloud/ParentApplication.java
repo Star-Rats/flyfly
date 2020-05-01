56c07230601ba39205675c80ee91ac787f2c3545
Star-Rats <54135730+Star-Rats@users.noreply.github.com>
2020-05-01 22:32:26 +0800

    Create ParentApplication.java

---
+++ SpringCloudNetflixDemo/src/main/java/com/jmy/cloud/ParentApplication.java
@@ -0,0 +1,13 @@
+package com.jmy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentApplication.class, args);
+    }
+
+}
