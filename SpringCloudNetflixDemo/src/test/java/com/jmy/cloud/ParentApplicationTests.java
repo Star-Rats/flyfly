bef86a9f465f9ed99cad471b0d8852fc798e2bd6
Star-Rats <54135730+Star-Rats@users.noreply.github.com>
2020-05-01 22:32:33 +0800

    Create ParentApplicationTests.java

---
+++ SpringCloudNetflixDemo/src/test/java/com/jmy/cloud/ParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmy.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
