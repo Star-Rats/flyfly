05650e44f78da019825d978bd91612fe43bac90b
Star-Rats <54135730+Star-Rats@users.noreply.github.com>
2020-05-01 22:31:27 +0800

    Create ServletInitializer.java

---
+++ SpringCloudNetflixDemo/Cloud_Provider/src/main/java/com/jmy/cloud/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.jmy.cloud;
+
+import com.jmy.cloud.ProviderApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProviderApplication.class);
+    }
+
+}
